117c3a45e05c9d27590e43a488fa147aa1229cbf
Swapnil Raj <raj.swapnil99@gmail.com>
2021-09-22 11:15:07 +0100

    Move fix for linking boost on macos to build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,10 @@
 
 mkdir -p build
 cd build
+# See https://github.com/Homebrew/homebrew-core/issues/67427#issuecomment-756511468
+# for an explanation why we adjust LIBRARY_PATH for macos
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  export LIBRARY_PATH="${LIBRARY_PATH}:$(brew --prefix icu4c)/lib"
+fi
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j20
