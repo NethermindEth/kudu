29acd1dce80068ca0ba27f1a7898fd0ea0fb7780
Artem Yurchenko <artemyurchenko@zoho.com>
2021-10-11 18:06:28 +0300

    get rid of bash-specific "[[" and "$OSTYPE"

--- build.sh
+++ build.sh
@@ -4,9 +4,11 @@ mkdir -p build
 cd build
 # See https://github.com/Homebrew/homebrew-core/issues/67427#issuecomment-756511468
 # for an explanation why we adjust LIBRARY_PATH for macos
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  export LIBRARY_PATH="${LIBRARY_PATH}:$(brew --prefix icu4c)/lib"
-fi
+case $(uname | tr '[:upper:]' '[:lower:]') in
+    darwin*)
+        export LIBRARY_PATH="${LIBRARY_PATH}:$(brew --prefix icu4c)/lib"
+        ;;
+esac
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j20
 status=$?
