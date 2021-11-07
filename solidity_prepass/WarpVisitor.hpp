54370416dd6b1faeb8d469553527fb107b22de3c
BitBaseBit <vardygreg23@gmail.com>
2021-11-07 10:15:58 +0000

    make indent 4 & rename SourceData to WarpVisitor

--- solidity_prepass/WarpVisitor.hpp
+++ solidity_prepass/WarpVisitor.hpp
@@ -14,101 +14,95 @@
 using namespace solidity::frontend;
 using namespace solidity;
 
-struct MarkedFunctions
-{
-	std::vector<std::string>			  names;
-	std::vector<std::string>			  selectors;
-	std::vector<std::vector<Type const*>> parameters;
+struct MarkedFunctions {
+    std::vector<std::string> names;
+    std::vector<std::string> selectors;
+    std::vector<std::vector<Type const*>> parameters;
 };
 
+class WarpVisitor : public ASTConstVisitor {
+   public:
+    enum class PassType {
+        AddrTypePass,
+        ConstructorPass,
+        FunctionCallPass,
+        FunctionDefinitionPass,
+        StorageVarPass,
+        StorageFunctionPass,
+    };
 
-class SourceData: public ASTConstVisitor
-{
-public:
-	enum class PassType
-	{
-		AddrTypePass,
-		ConstructorPass,
-		FunctionCallPass,
-		FunctionDefinitionPass,
-		StorageVarPass,
-		StorageFunctionPass,
-	};
+    WarpVisitor(std::string main_contract, std::string src, std::string filepath,
+               bool print_ir);
 
-	SourceData(std::string main_contract,
-			   std::string src,
-			   std::string filepath,
-			   bool		   print_ir);
+    CommandLineInterface getCli(char const* sol_filepath);
+    FunctionDefinition const* resolveFunctionCall(const ContractDefinition& c,
+                                                  FunctionCall const& f);
+    FunctionDefinition const* insideWhichFunction(
+        langutil::SourceLocation const& location);
+    void getInheritedConstructorCalls(
+        std::vector<ASTPointer<ModifierInvocation>> const& modifiers);
 
-	CommandLineInterface	  getCli(char const* sol_filepath);
-	FunctionDefinition const* resolveFunctionCall(const ContractDefinition& c,
-												  FunctionCall const&		f);
-	FunctionDefinition const*
-		 insideWhichFunction(langutil::SourceLocation const& location);
-	void getInheritedConstructorCalls(
-		std::vector<ASTPointer<ModifierInvocation>> const& modifiers);
+    std::string getSaferYul();
+    void removeComments();
+    bool visit(FunctionDefinition const& _node) override;
+    bool visit(FunctionCall const& _node) override;
+    bool visit(VariableDeclaration const& _node) override;
+    bool visit(Identifier const& _node) override;
+    bool visitNode(ASTNode const& node) override;
+    void prepareSoliditySource(const char* sol_filepath);
+    void setCompilerOptions(std::shared_ptr<CompilerStack> compiler);
+    void writeModifiedSolidity();
+    void dynFuncArgsPass(const char* solFilepath);
+    void constrcutorPass(const char* solFilepath);
+    void addressPass();
+    void addressTypePass();
+    void setYulOptimizerSettings();
+    void functionCallPass();
+    void refreshStateAfterModification();
+    void generateWarpConstructor();
 
-	std::string getSaferYul();
-	void		removeComments();
-	bool		visit(FunctionDefinition const& _node) override;
-	bool		visit(FunctionCall const& _node) override;
-	bool		visit(VariableDeclaration const& _node) override;
-	bool		visit(Identifier const& _node) override;
-	bool		visitNode(ASTNode const& node) override;
-	void		prepareSoliditySource(const char* sol_filepath);
-	void		setCompilerOptions(std::shared_ptr<CompilerStack> compiler);
-	void		writeModifiedSolidity();
-	void		dynFuncArgsPass(const char* solFilepath);
-	void		constrcutorPass(const char* solFilepath);
-	void		addressPass();
-	void		addressTypePass();
-	void		setYulOptimizerSettings();
-	void		functionCallPass();
-	void		refreshStateAfterModification();
-	void		generateWarpConstructor();
+    std::vector<std::pair<std::string, std::string>> m_addrMarkedFuncs;
+    std::vector<std::string> m_srcSplit;
+    std::vector<std::string> m_srcSplitOriginal;
+    boost::filesystem::path m_baseFileName;
 
-	std::vector<std::pair<std::string, std::string>> m_addrMarkedFuncs;
-	std::vector<std::string>						 m_srcSplit;
-	std::vector<std::string>						 m_srcSplitOriginal;
-	boost::filesystem::path							 m_baseFileName;
+    bool m_willGenerateConstructor = false;
+    std::string m_warpConstructor;
+    std::string m_warpConstructorName;
+    std::string m_warpConstructorSig;
+    std::string m_warpConstructorSelector;
+    std::string m_mainContract;
+    std::string m_currentFunction;
+    std::string m_modifiedContractName;
+    std::string m_currentFunctionModified;
+    std::vector<std::string> m_currentFunctionParams;
+    std::vector<std::string> m_contractNames;
+    std::vector<std::string> m_interfaceNames;
+    std::map<std::string, std::vector<std::string>> m_interfaces;
+    std::string m_filepath;
+    std::string m_modifiedSolFilepath;
+    std::string m_srcModified;
+    std::string m_src;
+    std::string m_srcOriginal;
+    bool m_print_ir;
 
-	bool					 m_willGenerateConstructor = false;
-	std::string				 m_warpConstructor;
-	std::string				 m_warpConstructorName;
-	std::string				 m_warpConstructorSig;
-	std::string				 m_warpConstructorSelector;
-	std::string				 m_mainContract;
-	std::string				 m_currentFunction;
-	std::string				 m_modifiedContractName;
-	std::string				 m_currentFunctionModified;
-	std::vector<std::string> m_currentFunctionParams;
-	std::vector<std::string> m_contractNames;
-	std::vector<std::string> m_interfaceNames;
-	std::map<std::string, std::vector<std::string>> m_interfaces;
-	std::string										m_filepath;
-	std::string										m_modifiedSolFilepath;
-	std::string										m_srcModified;
-	std::string										m_src;
-	std::string										m_srcOriginal;
-	bool											m_print_ir;
+    std::shared_ptr<CompilerStack> m_compiler;
+    OptimiserSettings m_compilerOptimizerSettings;
+    FileReader m_fileReader;
+    std::shared_ptr<CommandLineInterface> m_cli;
+    CommandLineOptions m_options;
+    std::vector<FunctionDefinition const*> m_definedFunctions;
+    std::vector<std::string> m_storageVars_str;
+    std::vector<const VariableDeclaration*> m_storageVars_astNodes;
+    PassType m_currentPass;
 
-	std::shared_ptr<CompilerStack>			m_compiler;
-	OptimiserSettings						m_compilerOptimizerSettings;
-	FileReader								m_fileReader;
-	std::shared_ptr<CommandLineInterface>	m_cli;
-	CommandLineOptions						m_options;
-	std::vector<FunctionDefinition const*>	m_definedFunctions;
-	std::vector<std::string>				m_storageVars_str;
-	std::vector<const VariableDeclaration*> m_storageVars_astNodes;
-	PassType								m_currentPass;
+   private:
+    bool isPublic(Visibility _visibility);
+    bool hasDynamicArgs(std::string params);
+    bool checkTypeEqaulity(std::vector<Type const*> const& t1,
+                           std::vector<Type const*> t2);
 
-private:
-	bool isPublic(Visibility _visibility);
-	bool hasDynamicArgs(std::string params);
-	bool checkTypeEqaulity(std::vector<Type const*> const& t1,
-						   std::vector<Type const*>		   t2);
-
-	std::vector<std::string> m_storageVars;
-	std::vector<std::string> m_hashNames;
-	MarkedFunctions			 m_dynArgFunctions;
+    std::vector<std::string> m_storageVars;
+    std::vector<std::string> m_hashNames;
+    MarkedFunctions m_dynArgFunctions;
 };
--- yul_prepass/YulVisitor.hpp
+++ yul_prepass/YulVisitor.hpp
@@ -1,57 +1,54 @@
 #pragma once
 
-#include <boost/variant.hpp>
 #include <liblangutil/SourceLocation.h>
 #include <libyul/AST.h>
+
+#include <boost/variant.hpp>
 #include <optional>
 #include <vector>
 
 using namespace solidity;
 
+class YulVisitor : public boost::static_visitor<void> {
+   public:
+    enum class PassType {
+        CaseRemoval,
+        Constructor,
+        AddrCleanerRemoval,
+        Skip,
+    };
 
-class YulVisitor: public boost::static_visitor<void>
-{
-public:
-	enum class PassType
-	{
-		CaseRemoval,
-		Constructor,
-		AddrCleanerRemoval,
-		Skip,
-	};
+    YulVisitor(std::string src, std::string warpConstructorSelector,
+               std::string warpConstructorName);
 
-	YulVisitor(std::string src,
-			   std::string warpConstructorSelector,
-			   std::string warpConstructorName);
+    template <class T>
+    void visitVectorOfVariants(const std::vector<T>& _vec);
+    void printSrcAtLocation(langutil::SourceLocation loc);
+    void writeModifiedYul();
+    void refreshYulAST();
 
-	template<class T>
-	void visitVectorOfVariants(const std::vector<T>& _vec);
-	void printSrcAtLocation(langutil::SourceLocation loc);
-	void writeModifiedYul();
-	void refreshYulAST();
+    void operator()(const yul::Block& _node);
+    void operator()(const yul::TypedName& _node);
+    void operator()(const yul::Literal& _node);
+    void operator()(const yul::Identifier& _node);
+    void operator()(const yul::Assignment& _node);
+    void operator()(const yul::VariableDeclaration& _node);
+    void operator()(const yul::FunctionDefinition& _node);
+    void operator()(const yul::FunctionCall& _node);
+    void operator()(const yul::If& _node);
+    void operator()(const yul::Switch& _node);
+    void operator()(const yul::Case& _node);
+    void operator()(const yul::ForLoop& _node);
+    void operator()(const yul::Break& _node);
+    void operator()(const yul::Continue& _node);
+    void operator()(const yul::Leave& _node);
+    void operator()(const yul::ExpressionStatement& _node);
 
-	void operator()(const yul::Block& _node);
-	void operator()(const yul::TypedName& _node);
-	void operator()(const yul::Literal& _node);
-	void operator()(const yul::Identifier& _node);
-	void operator()(const yul::Assignment& _node);
-	void operator()(const yul::VariableDeclaration& _node);
-	void operator()(const yul::FunctionDefinition& _node);
-	void operator()(const yul::FunctionCall& _node);
-	void operator()(const yul::If& _node);
-	void operator()(const yul::Switch& _node);
-	void operator()(const yul::Case& _node);
-	void operator()(const yul::ForLoop& _node);
-	void operator()(const yul::Break& _node);
-	void operator()(const yul::Continue& _node);
-	void operator()(const yul::Leave& _node);
-	void operator()(const yul::ExpressionStatement& _node);
-
-	bool					   m_inEntryFunction;
-	bool					   m_dynamicArgsInConstructor;
-	PassType				   m_currentPass;
-	std::string				   m_src;
-	const std::string		   dynamicEntrySeq = R"(
+    bool m_inEntryFunction;
+    bool m_dynamicArgsInConstructor;
+    PassType m_currentPass;
+    std::string m_src;
+    const std::string dynamicEntrySeq = R"(
                 let _1 := 128
                 let _2 := 64
                 mstore(_2, _1)
@@ -61,13 +58,13 @@ public:
 				let _8 := calldataload(_7)
 				let _9 := 224
 				let _10 := shr(_9, _8) )";
-	std::string				   m_srcModified;
-	std::optional<std::string> m_deletedCase;
-	std::string				   m_currentFunction;
-	std::string				   m_currentDeclr		 = "";
-	int						   m_currDeclrLocation[2] = {0, 0};
-	int						   m_prevDeclrLocation[2];
-	std::string				   m_prevDeclr;
-	std::string				   m_constructorName;
-	std::string				   m_constructorSelector;
+    std::string m_srcModified;
+    std::optional<std::string> m_deletedCase;
+    std::string m_currentFunction;
+    std::string m_currentDeclr = "";
+    int m_currDeclrLocation[2] = {0, 0};
+    int m_prevDeclrLocation[2];
+    std::string m_prevDeclr;
+    std::string m_constructorName;
+    std::string m_constructorSelector;
 };
