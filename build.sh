3855412401e41c9d1f26da0adfe5b2447b15c7b9
Artem Yurchenko <artemyurchenko@zoho.com>
2021-11-22 18:37:20 +0300

    remove bashisms, add shellcheck in CI

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env sh
 
 mkdir -p build
-cd build
+cd build || exit
 # See https://github.com/Homebrew/homebrew-core/issues/67427#issuecomment-756511468
 # for an explanation why we adjust LIBRARY_PATH for macos
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  export LIBRARY_PATH="${LIBRARY_PATH}:$(brew --prefix icu4c)/lib"
-fi
+case "$(uname -s)" in
+    Darwin*)
+        LIBRARY_PATH="${LIBRARY_PATH}:$(brew --prefix icu4c)/lib"
+        export LIBRARY_PATH
+        ;;
+esac
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j"$(getconf _NPROCESSORS_ONLN)"
 status=$?
